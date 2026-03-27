44baa78548b8f1aa353dfc79288966e19d59f54d
Wolffire88 <robbie.bana.13@gmail.com>
2026-03-26 21:52:26 -0400

    formatting

--- src/Tokenizer.c
+++ src/Tokenizer.c
@@ -7,6 +7,14 @@ void copyupto(char* dest, const char* src, int index) {
     }
 }
 
+int iswhitespace(char c) {
+    return (
+        c == ' '  ||
+        c == '\n' ||
+        c == '\t'
+    );
+}
+
 int isnumeric(char c) {
     return (c - '0' <= 9 && c - '0' >= 0);
 }
@@ -17,29 +25,28 @@ int len(char* str) {
     return count;
 }
 
-void pause() {
-    char _;
-    scanf("%c", _);
-}
-
 /* GET TOKEN */
 /*
 Grab the first token from input stream, using whitespaces as delimiters
 We then wipe the token from the input stream, starting the string at a new character.
 */
-
 char* gettoken(char** progstring) {
-    if (!len(*progstring)) return NULL;
-    char* start = *progstring;
+    if (!len(*progstring)) return NULL;         //Leave early if no program left
+
+    char* start = *progstring;                  //Begin the things
     char* retpreserve = (char*)malloc(10000*sizeof(char));
     char* ret = retpreserve;
-    if (isnumeric(*start)) {
+
+    while (iswhitespace(*start)) start++;       //Skip past whitespacess
+
+    if (isnumeric(*start)) {                    //Check for int literals
         while (isnumeric(*start)) {
             *ret = *start;
             ret++;
             start++;
         }
-    } else if (*start == '\"') {
+
+    } else if (*start == '\"') {                //Check for strings
         start++;
         while (*start != '\"') {
             *ret = *start;
@@ -47,10 +54,12 @@ char* gettoken(char** progstring) {
             start++;
         }
         start++;
-    } else if (*start == '\'') {
+
+    } else if (*start == '\'') {                //Check for character codes
         start++;
         itoa(*start, ret, 10);
-    } else if (*start == '[') {
+
+    } else if (*start == '[') {                 //Check for lambdas
         start++;
         while (*start != ']') {
             *ret = *start;
@@ -58,12 +67,15 @@ char* gettoken(char** progstring) {
             start++;
         }
         start++;
-    } else {
+
+    } else {                                    //Save everything else as a character
         *ret = *start;
         start++;
         ret++;
+
     }
-    *ret = 0;
-    *progstring = start;
-    return retpreserve;
+
+    *ret = 0;                                   //Denote end of retstring
+    *progstring = start;                        //Move start location
+    return retpreserve;                         //Return the token
 }
