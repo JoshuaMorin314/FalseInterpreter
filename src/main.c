8c2091422feadb7f1a5e2fda8fd7e734a3409f04
Wolffire88 <robbie.bana.13@gmail.com>
2026-03-28 03:38:25 -0400

    testing new features; removed joke names

--- src/main.c
+++ src/main.c
@@ -36,24 +36,78 @@ void teststack(){
 }
 */
 
-int main(){
-    char* programstring = "1 67 333 09123721 839826 a:a;";
-    char** hahareference = &programstring;
+void processvariable(Node** stack, char var, char fun);
+void processlexeme(Node** stack, char lex);
 
-    Data funny;
-    funny = gettoken(hahareference);
-    while (funny.tag == TYPE_STR && funny.val.string != NULL || funny.tag == TYPE_INT) {
-        switch(funny.tag) {
+int main(){
+    Node** stack = create_stack();
+    char* progstr = "123 44 a: 44 3 ;";
+    char** progref = &progstr;
+
+    Data token;
+    token = gettoken(progref);
+    while (token.tag == TYPE_STR && token.val.string != NULL || 
+            token.tag == TYPE_INT || token.tag == TYPE_FUN) {
+        switch(token.tag) {
             case TYPE_STR:
-                printf("%s\n", funny.val.string);
+                if (len(token.val.string) > 2) {
+                    printf("%s", token.val.string);
+                } else if (len(token.val.string) == 2) {
+                    processvariable(stack, token.val.string[0], token.val.string[1]);
+                } else if (isalphabetic(*token.val.string)) {
+                    push(stack, token);
+                } else {
+                    processlexeme(stack, *token.val.string);
+                }
                 break;
+            case TYPE_FUN:
             case TYPE_INT:
-                printf("%d\n", funny.val.integer);
+                push(stack, token);
                 break;
         }
 
-        funny = gettoken(hahareference);
+        token = gettoken(progref);
     }
 
+    print_stack(stack);
+
     return 0; 
+}
+
+void processvariable(Node** stack, char var, char fun) {
+    switch (fun) {
+        /* variable functions */
+        case ':':           //Store top into a variable
+            store(stack, var);
+            break;
+        case ';':           //Fetch from a variable
+            fetch(stack, var);
+            break;
+    }
+}
+
+void processlexeme(Node** stack, char lex) {
+    switch(lex) {
+        /* stack functions */
+        case '$':           //Duplicate top of stack (equivelant to pick(0))
+            DUP(stack);
+            break;
+        case '%':           //Pop top item from stack
+            DROP(stack);
+            break;
+        case '\\':          //Swap top and top->next
+            SWAP(stack);
+            break;
+        case '@':           //Bring third item to top
+            ROT(stack);
+            break;
+        case 'U':           //C doesn't like greek letters so I'm improvising
+            PICK(stack);    //Pick the nth item from the stack (starting from 0)
+            break;
+
+        case ';':
+        case ':':
+            printf("Error: Fetch/store called with no associated variable");
+            exit(EXIT_FAILURE);
+    }
 }
